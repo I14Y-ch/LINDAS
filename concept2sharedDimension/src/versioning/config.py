df7e2ff8b7202d1a79c6236e2fff139911e26d6f
fabian-santi <fabian.santi@bfs.admin.ch>
2025-09-04 09:44:33 +0200

    Update config.py
    Corrected data issue on FAHRZEUGART concept ( "08dad447-fdc0-073a-b3f5-b70f4b876faf") so it can be included again

--- concept2sharedDimension/src/versioning/config.py
+++ concept2sharedDimension/src/versioning/config.py
@@ -11,8 +11,7 @@ STATUSES = ["Qualified",  "Standard", "PreferredStandard"]
 # 21.08.2025: I have removed NOGA single hierarchy concepts (NOGA Section, ...) they are duplicates of NOGA
 # 21.08.2025: I have removed Isco Jobs related concept
 # 22.08.2025: I have removed EprAgentRole codelist (08dd632d-aee2-333d-b1e4-505385fde8ff) because it gets an internal error 500 when retrieving codelist entries from I14Y
-# 28.08.2025: I have removed FAHRZEUGART, KAROSSERIEFORM concept ( "08dad447-fdc0-073a-b3f5-b70f4b876faf") because there is a space in the URI of the field conformsTo. 
-EXCLUDED_IDS = [ "08dad447-fdc0-073a-b3f5-b70f4b876faf", "08dd632d-aee2-333d-b1e4-505385fde8ff", "08dd20cc-3ee5-c17a-8d8b-c1664c3421a6", "08dc74b4-30d4-be8d-ba53-91a1201ac86b", "08dceec4-b3f0-1285-b8d0-b005fabcf87c", "08d92cdc-c97d-acd7-9952-56dd375b0777", "08d92cdc-827b-5021-8242-f4166fb122b0", "08d92cdc-03c1-103f-8fec-a24aafeaedd3", "08d92cdc-4e4a-5b21-9e0e-a50ffe3f4a16", "08d92cdc-4e4a-5b21-9e0e-a50ffe3f4a16", "08d94603-f490-f094-9fe5-8012ca56d812", "08d94604-db03-1909-a097-569cb9836253", "08d94604-f12a-5d82-b6b4-fdc55b9b4750", "08d94604-e058-62a2-aa25-53f84b974201", "08d94604-e5ac-f859-9ed7-f51fe87baa25", "08da9d6a-bc1f-d917-bb12-5c78e7b8bc0f"] 
+EXCLUDED_IDS = [ "08dd632d-aee2-333d-b1e4-505385fde8ff", "08dd20cc-3ee5-c17a-8d8b-c1664c3421a6", "08dc74b4-30d4-be8d-ba53-91a1201ac86b", "08dceec4-b3f0-1285-b8d0-b005fabcf87c", "08d92cdc-c97d-acd7-9952-56dd375b0777", "08d92cdc-827b-5021-8242-f4166fb122b0", "08d92cdc-03c1-103f-8fec-a24aafeaedd3", "08d92cdc-4e4a-5b21-9e0e-a50ffe3f4a16", "08d92cdc-4e4a-5b21-9e0e-a50ffe3f4a16", "08d94603-f490-f094-9fe5-8012ca56d812", "08d94604-db03-1909-a097-569cb9836253", "08d94604-f12a-5d82-b6b4-fdc55b9b4750", "08d94604-e058-62a2-aa25-53f84b974201", "08d94604-e5ac-f859-9ed7-f51fe87baa25", "08da9d6a-bc1f-d917-bb12-5c78e7b8bc0f"] 
 # If using specific concept IDs
 CONCEPT_IDS = ["08dd28d2-a693-5049-a3fe-0ee83005b61b"]
 
@@ -40,6 +39,7 @@ dataCite = Namespace("https://datacite-metadata-schema.readthedocs.io/en/4.6/app
 ADMS = Namespace("https://www.w3.org/TR/vocab-adms/#adms_")
 RDFA = Namespace("https://www.w3.org/ns/rdfa#")
 PAV = Namespace("http://purl.org/pav/")
+
 
 
 
